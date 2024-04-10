fd6cec31dd1e485bdf774e3169c989ff37cbf189
chungchung <cndgks0477@naver.com>
2024-04-10 22:49:37 +0900

    create priority module:

---
+++ kafka/b-priority/src/main/java/org/example/bpriority/BPriorityApplication.java
@@ -0,0 +1,13 @@
+package org.example.bpriority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BPriorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BPriorityApplication.class, args);
+    }
+
+}
---
+++ kafka/b-priority/src/test/java/org/example/bpriority/BPriorityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bpriority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BPriorityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
